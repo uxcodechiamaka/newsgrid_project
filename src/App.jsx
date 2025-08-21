0ba2ff01c80d91f743bee3e42c58c8a32d641dbb
uxcodechiamaka <chiamakanwankwo557@gmail.com>
2025-08-21 16:50:14 +0000

    LatestNews

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import LatestNews from "./components/LatestNews";
 
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <div>
         <Header  />
         <Navbar  />
+        <LatestNews  />
     </div>
 
     
---
+++ src/components/LatestNews.jsx
@@ -0,0 +1,55 @@
+export default function LatestNews() {
+  return (
+    <div className="px-4">
+      {/* Title with blue square */}
+      <div className="flex items-center gap-2 font-medium">
+        <div className="w-3 h-3 bg-blue-600"></div>
+        <div>Latest News</div>
+      </div>
+
+      <div className="my-4"></div>
+
+      {/* Scrollable container */}
+      <div className="w-[17rem] h-[27rem] overflow-y-scroll border border-gray-300 rounded-lg p-2">
+        {/* Card 1 */}
+        <div className="w-full h-[8rem] border border-gray-400 bg-gray-100 rounded-[8px] shadow-md p-4 mb-4">
+          <div className="text-[#00356A] italic">8 minutes ago</div>
+          <div className="my-2"></div>
+          <div>
+            Trump ‘has power to force Putin’ into peace talks, says EU chief
+          </div>
+        </div>
+
+        {/* Card 2 */}
+        <div className="w-full h-[8rem] border border-gray-400 bg-gray-100 rounded-[8px] shadow-md p-4 mb-4">
+          <div className="text-[#00356A] italic">8 minutes ago</div>
+          <div className="my-2"></div>
+          <div>
+            Trump ‘has power to force Putin’ into peace talks, says EU chief
+          </div>
+        </div>
+
+        {/* Card 3 */}
+        <div className="w-full h-[8rem] border border-gray-400 bg-gray-100 rounded-[8px] shadow-md p-4 mb-4">
+          <div className="text-[#00356A] italic">8 minutes ago</div>
+          <div className="my-2"></div>
+          <div>
+            Trump ‘has power to force Putin’ into peace talks, says EU chief
+          </div>
+        </div>
+
+        {/* Card 4 */}
+        <div className="w-full h-[8rem] border border-gray-400 bg-gray-100 rounded-[8px] shadow-md p-4">
+          <div className="text-[#00356A] italic">8 minutes ago</div>
+          <div className="my-2"></div>
+          <div>
+            Trump ‘has power to force Putin’ into peace talks, says EU chief
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+}
+
+
