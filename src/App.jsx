b8b5214692dbbe135a91d30d3236c0444e160429
uxcodechiamaka <chiamakanwankwo557@gmail.com>
2025-08-23 23:26:57 +0000

    News

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import LatestNews from "./components/LatestNews";
-import TopNews from "./components/TopNews";
-import Main from "./components/Main";
-
+import News from "./News";
 
 export default function App() {
-  return (
-    <div className="flex flex-col">
-        <Header  />
-        <Navbar  />
-        <div className="flex flex-row justify-between">
-            <LatestNews  />
-            <div className="flex-wrap">
-              <Main  />
-            <Main  />
-            </div>
-            
-            <TopNews  />
+      return(
+        <div>
+          <Header  />
+          <Navbar  />
+          <News  />
         </div>
-        
-      
-    </div>
-
-    
-      
-      
-    
-  );
+      )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,28 @@
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import LatestNews from "./components/LatestNews";
+import TopNews from "./components/TopNews";
+import Main from "./components/Main";
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+        <Header  />
+        <Navbar  />
+        <div className="flex flex-row justify-between">
+            <LatestNews  />
+            <div className="flex-wrap">
+              <Main  />
+            <Main  />
+            </div>
+            <TopNews  />
+        </div>
+    </div>
+
+    
+      
+      
+    
+  );
+}
